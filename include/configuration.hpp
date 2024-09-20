a2d7302b8f2d79ad5fec4a0d7c1c2abcd282330c
azzamjhd <71210715+azzamjhd@users.noreply.github.com>
2024-09-20 09:08:40 +0700

    chore: Update GPIO pin configuration for motors

--- include/configuration.hpp
+++ include/configuration.hpp
@@ -8,16 +8,16 @@
 // -----------------------------------------------------------------------------
 
 // GPIO pin configuration for the right motor.
-#define GPIO_MOTOR_RIGHT_EN 10  // Must be a PWM pin
-#define GPIO_MOTOR_RIGHT_IN1 6
-#define GPIO_MOTOR_RIGHT_IN2 7
+#define GPIO_MOTOR_RIGHT_EN 11  // Must be a PWM pin
+#define GPIO_MOTOR_RIGHT_IN1 9
+#define GPIO_MOTOR_RIGHT_IN2 10
 #define GPIO_MOTOR_RIGHT_ENCODER_A 3  // Must be a hardware interrupt pin
 #define GPIO_MOTOR_RIGHT_ENCODER_B 5
 
 // GPIO pin configuration for the left motor.
-#define GPIO_MOTOR_LEFT_EN 11  // Must be a PWM pin
+#define GPIO_MOTOR_LEFT_EN 6  // Must be a PWM pin
 #define GPIO_MOTOR_LEFT_IN1 8
-#define GPIO_MOTOR_LEFT_IN2 9
+#define GPIO_MOTOR_LEFT_IN2 7
 #define GPIO_MOTOR_LEFT_ENCODER_A 2  // Must be a hardware interrupt pin
 #define GPIO_MOTOR_LEFT_ENCODER_B 4
 
@@ -45,6 +45,6 @@
 
 // If you change this and want to use platformIO monitor, you must also change
 // the baud rate in platformio.ini
-#define SERIAL_BAUD_RATE 9600
+#define SERIAL_BAUD_RATE 115200
 
 #endif  // !CONFIGURATION_HPP
